03ac978d568693138731e8b364f82108e074df96
IssacYoung2013 <issacyoung@msn.cn>
2018-08-04 14:37:52 +0800

    验证tomcat 集群 session

--- src/main/java/com/imall/controller/portal/UserController.java
+++ src/main/java/com/imall/controller/portal/UserController.java
@@ -45,11 +45,9 @@ public class UserController {
     public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
-//            session.setAttribute(Const.CURRENT_USER, response.getData());
-//            516701822D3051D1CE752F2969AE838C
             CookieUtil.writeLoginToken(httpServletResponse,session.getId());
-            CookieUtil.readLoginToken(httpServletRequest);
-            CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
+//            CookieUtil.readLoginToken(httpServletRequest);
+//            CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return response;
--- src/main/java/com/imall/util/CookieUtil.java
+++ src/main/java/com/imall/util/CookieUtil.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Slf4j
 public class CookieUtil {
-    private final static String COOKIE_DOMAIN = ".issacmall.com";
+    private final static String COOKIE_DOMAIN = "www.issacmall.com";
     private final static String COOKIE_NAME = "imall_login_token";
 
     public static String readLoginToken(HttpServletRequest request) {
@@ -32,14 +32,21 @@ public class CookieUtil {
         return null;
     }
 
+    // a: A.issacmall.com cookie.domain=A.issacmall.com path="/"
+    // b: B.issacmall.com cookie.domain=B.issacmall.com path="/"
+    // c: A.issacmall.com/test/cc cookie.domain=A.issacmall.com path="/test/cc"
+    // d: A.issacmall.com/test/dd cookie.domain=A.issacmall.com path="/test/dd"
+    // e: A.issacmall.com/test cookie.domain=A.issacmall.com path="/test"
     public static void writeLoginToken(HttpServletResponse response,String token) {
         Cookie ck = new Cookie(COOKIE_NAME,token);
         ck.setDomain(COOKIE_DOMAIN);
+        ck.setHttpOnly(true); // 不允许脚本访问
         ck.setPath("/"); // 代表设置在根目录
         //单位是秒
         //如果 maxage不设置的话，cokkie就不会写入硬盘，而是写在内存。只在当前页面有效
         ck.setMaxAge(60 * 60 *24 *364); // 如果是-1 代表永久
         log.info("write cookieName:{},cookeValue:{}",ck.getName(),ck.getValue());
+        response.addCookie(ck);
     }
 
     public static void delLoginToken(HttpServletRequest request,HttpServletResponse response) {
